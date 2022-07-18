a06d31fa0a05a1a973d2eeee182c67aef93d4d08
Lam <vtlam.caha@gmail.com>
2022-07-18 14:27:56 +0700

    lab03.2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
-import "./App.css";
+import { DISHES } from "./shared/dishes";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/"> Ristorante con Funsion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
-    );
+    return <Menu dishes={this.state.dishes} />;
   }
 }
 

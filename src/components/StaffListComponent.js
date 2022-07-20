e75e55f35f3b5a7942ac50ea57410af81ced4322
Lam <vtlam.caha@gmail.com>
2022-07-20 14:32:34 +0700

    add asm1

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -2,22 +2,26 @@ import React, { Component } from "react";
 import { STAFFS } from "../shared/staffs";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 import Staff from "./StaffComponent";
-
+import App from "../App";
 class StaffList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedSaffs: null,
+      classDefault: "col-12 col-md-3 m-1",
     };
   }
   onStasffSelect(staff) {
     this.setState({ selectedSaffs: staff });
   }
+  col(a) {
+    this.setState({ classDefault: a });
+  }
   render() {
     console.log(this.state.selectedSaffs);
     const menu = this.props.staffs.map((staff) => {
       return (
-        <div className="col-12 col-md-5 m-1" key={staff.id}>
+        <div className={this.state.classDefault} key={staff.id}>
           <Card onClick={() => this.onStasffSelect(staff)}>
             <CardTitle>{staff.name}</CardTitle>
           </Card>
@@ -27,7 +31,10 @@ class StaffList extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <p>Bấm để xem thông tin</p>;
+        <button onClick={() => this.col("col-12 col-lg-6 m-1")}>1Cột</button>
+        <button onClick={() => this.col("col-12 col-lg-4 m-1")}>2 Cột</button>
+        <button onClick={() => this.col("col-12 col-lg-3 m-1")}>3 Cột</button>
+        <p onClick={this.shoot}>Bấm để xem thông tin</p>;
         <div className="row">
           <Staff staff={this.state.selectedSaffs} />
         </div>
